bf852ea06e8eb665e8897039b59a3526899ec615
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2023-06-24 23:52:43 -0300

    Update Business_bank.py

--- Business_bank.py
+++ Business_bank.py
@@ -1,44 +1,151 @@
-conta = 4578
-senha = 1234
-menu = ' '
-conta_saldo = float(100)
+#Bloco de variaveis 
+
+cadastro = ''
+novo_cad = ''
+cpf = '123065410'# CPF padrão quando o usuário possui cadastro
+senha = '123456' #senha padrão quando o usuário possui cadastro
+novo_cpf = ''
+nova_senha = ''
+nome = ''
+menu = ''
+saldo = float(0)
+perfil = ''
+autenticacao = ''
+conta = '58740'
+valor = ''
 
 
 
-conta = int(input('Digite a sua conta: '))
-senha = int(input('Digite a sua senha: '))
-
-if conta == 4578 and senha == senha:
-    print('Login efetuado com sucesso!')
-elif conta == ' ' and senha != senha:
-    print('Erro: Tente novamente!')    
-
-print('-'*40)
-
-menu = int(input('Qual opção deseja acessar: Área Pix[1] Fatura[2] Conta[3]: '))
-
-if menu == 3:
-    print(f'O seu saldo disponivel é de R${conta_saldo:.2f} reias')
-elif menu == 2:
-    print('Resumo da fatura:'
-          '\n 13 ABR. Oxxo Dumas        R$  6,48'
-          '\n 13 ABR. Padaria mercearia R$ 17,00')
 
 
-print('-'*40)   
 
-retornar = int(input('Deseja retornar ao Menu? Sim[1] Não[2]'))
+while True:
+    
+    entrada = input('[E]ntrar [S]air: ')
+    
+    if entrada == 'E' or entrada == 'e':
+        print('Entrando no sistema.')
+        
+    elif entrada == 'S' or entrada == 's': 
+        print('Acesso encerrado.')
 
-if retornar == 1:
-    menu = int(input('Qual opção deseja acessar: Área Pix[1] Fatura[2] Conta[3]: '))
-elif retornar == 2:
-    print(f'O seu saldo disponivel é de R${conta_saldo:.2f} reias')    
-
-print('-'*40)
-
-if menu == 2:
-    print('Resumo da fatura:'
-          '\n 13 ABR. Oxxo Dumas        R$  6,48'
-          '\n 13 ABR. Padaria mercearia R$ 17,00')
-elif menu == 3:
-    print(f'O seu saldo disponivel é de R${conta_saldo:.2f} reias')    
+    while True:
+        cadastro = input('Você possui uma conta? sim ou não: ') 
+              
+        print('-'*50)
+              
+        if cadastro == 'sim' or cadastro =='Sim':
+            nome = input('Digite o seu nome: ')
+            novo_cpf = input('Digite o seu CPF: ')
+            nova_senha = input('Digite a sua senha: ')
+                  
+            if novo_cpf == novo_cpf and nova_senha == nova_senha:
+                print('Login efetuado com sucesso!')
+                
+                while True:
+                     
+                    menu = input('Qual opção deseja acessar? [c]onta [t]ransferência [d]epósito [s]air: ')
+                           
+                          
+                    if menu == 'c':
+                        print(f'O saldo da sua conta é R${saldo:.2f}')
+                                
+                        print('-'*50)
+                                
+                                
+                    if menu == 'd':
+                        print('Para realizar um Depósito em sua conta, preencha o formulario: ')
+                                    
+                        print('-'*50)
+                                    
+                        print('Gerando boleto...')
+                                    
+                        print('-'*50)
+                                    
+                        valor = int(input('Digite o valor que deseja depósitar: '))
+                                    
+                        print(f'Boleto no valor de R${valor:.2f} gerado, prazo para compensação de até 3 dias úteis.')
+                            
+                        saldo = saldo + valor
+                            
+                    elif menu == 's':
+                        print('Acesso encerrado...')
+                        break
+                    continue
+                
+                
+                
+            else:
+                print('Erro ao efetuar o login, tente novamente!')
+                continue
+            break
+                 
+              
+        elif cadastro == 'não' or cadastro == 'Não':
+                  
+            novo_cad = input('Deseja criar uma nova conta: sim ou não: ')
+                  
+            if novo_cad == 'sim' and 'Sim':
+                nome = input('Digite o seu nome: ')
+                novo_cpf = input('Digite o seu CPF: ')
+                nova_senha = input('Digite a sua senha: ')
+                      
+                     
+                print('Cadastro registrado com sucesso!')
+                break
+                      
+    print('-'*50)
+                      
+                      
+    while True:
+                          
+        print('Faça o Login da sua conta: ')
+                          
+        novo_cpf = input('Digite o seu CPF: ')
+        nova_senha = input('Digite a sua senha: ')
+                          
+        if novo_cpf == novo_cpf and nova_senha == nova_senha:
+            print(f'{nome} Bem vindo a sua conta no Business bankt!')
+                
+            print('-'*50)
+                    
+        else:
+            print('Erro ao efetuar o login, tente novamente!')
+            continue
+        break
+                
+                      
+        print('-'*50)
+                      
+                      
+                      
+    while True:
+                     
+        menu = input('Qual opção deseja acessar? [c]onta [t]ransferência [d]epósito [s]air: ')
+               
+              
+        if menu == 'c':
+            print(f'O saldo da sua conta é R${saldo:.2f}')
+                    
+            print('-'*50)
+                    
+                    
+        if menu == 'd':
+            print('Para realizar um Depósito em sua conta, preencha o formulario: ')
+                        
+            print('-'*50)
+                        
+            print('Gerando boleto...')
+                        
+            print('-'*50)
+                        
+            valor = int(input('Digite o valor que deseja depósitar: '))
+                        
+            print(f'Boleto no valor de R${valor:.2f} gerado, prazo para compensação de até 3 dias úteis.')
+                
+            saldo = saldo + valor
+                
+        elif menu == 's':
+            print('Acesso encerrado...')
+            break
+        continue
